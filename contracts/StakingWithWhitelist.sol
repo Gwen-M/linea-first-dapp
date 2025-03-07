b2b237f4ee208db521e77de2f16e015b9e8c5b0b
Gwen-M <gwenole19@gmail.com>
2025-03-07 19:48:18 +0100

    small contract changes

--- contracts/StakingWithWhitelist.sol
+++ contracts/StakingWithWhitelist.sol
@@ -3,21 +3,21 @@ pragma solidity ^0.8.24;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract StakingWithWhitelist {
+contract StakingWithAllowlist {
     mapping(address => mapping(address => uint256)) public stakedBalances;
-    mapping(address => bool) public whitelistedTokens;
+    mapping(address => bool) public allowlistedTokens;
 
     event Staked(address indexed user, address indexed token, uint256 amount);
     event Withdrawn(address indexed user, address indexed token, uint256 amount);
 
     constructor(address[] memory tokens) {
         for (uint256 i = 0; i < tokens.length; i++) {
-            whitelistedTokens[tokens[i]] = true;
+            allowlistedTokens[tokens[i]] = true;
         }
     }
 
     function stake(address _token, uint256 _amount) public {
-        require(whitelistedTokens[_token], "Token is not whitelisted");
+        require(allowlistedTokens[_token], "Token is not in the allowlist");
         require(_amount > 0, "Amount must be greater than 0");
         IERC20 token = IERC20(_token);
         require(token.transferFrom(msg.sender, address(this), _amount), "Token transfer failed");
@@ -26,7 +26,7 @@ contract StakingWithWhitelist {
     }
 
     function withdraw(address _token, uint256 _amount) public {
-        require(whitelistedTokens[_token], "Token is not whitelisted");
+        require(allowlistedTokens[_token], "Token is not in the allowlist");
         require(_amount > 0, "Amount must be greater than 0");
         require(stakedBalances[msg.sender][_token] >= _amount, "Insufficient staked balance");
         IERC20 token = IERC20(_token);
@@ -35,7 +35,7 @@ contract StakingWithWhitelist {
         emit Withdrawn(msg.sender, _token, _amount);
     }
 
-    function isTokenWhitelisted(address _token) public view returns (bool) {
-        return whitelistedTokens[_token];
+    function isTokenAllowlisted(address _token) public view returns (bool) {
+        return allowlistedTokens[_token];
     }
 }
